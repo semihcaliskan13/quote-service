550086d9ff3cae6da76562d0daad439899447fd0
semih <semihcaliskan0103@gmail.com>
2024-01-20 16:16:27 +0300

    added aop to create user profile when user sign in

---
+++ src/main/java/com/land/quotebackend/aop/CreateUserProfileAspect.java
@@ -0,0 +1,37 @@
+package com.land.quotebackend.aop;
+
+import com.land.quotebackend.entity.User;
+import com.land.quotebackend.entity.UserProfile;
+import com.land.quotebackend.service.UserProfileService;
+import com.land.quotebackend.service.UserService;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class CreateUserProfileAspect {
+
+    private final UserProfileService _userProfileService;
+    private final UserService _userService;
+
+    public CreateUserProfileAspect(UserProfileService userProfileService, UserService userService) {
+        _userProfileService = userProfileService;
+        _userService = userService;
+    }
+
+    @Pointcut("execution(* com.land.quotebackend.service.impl.UserServiceImpl.saveUser(..))")
+    public void saveUserMethod() {
+
+    }
+
+    @AfterReturning(pointcut = "saveUserMethod()", returning = "returnValue")
+    public void createUserProfileAfterUserSaved(Object returnValue){
+        if(returnValue instanceof User user){
+            UserProfile userProfile = new UserProfile(null,null,null,null,user);
+            _userProfileService.createUserProfile(userProfile);
+        }
+    }
+}
